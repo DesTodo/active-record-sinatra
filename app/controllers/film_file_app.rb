3acdfc92529202af1a06fabf8a2b9b98447d2042
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-28 15:08:34 -0600

    fixed bug in class FilmFile; view renders in browser

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -1,14 +1,5 @@
 class FilmFile < Sinatra::Base
-  get '/films' do
-    erb :films_index
-  end
 
-  get '/genres' do
-    @genres = Genre.all
-    erb :
-end
-
-class FilmFile < Sinatra::Base
   get '/genres' do
     @genres = Genre.all
     erb :genres_index
