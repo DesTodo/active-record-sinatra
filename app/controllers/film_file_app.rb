820d7e0a56c13a27b26e427adaea5574aa1115be
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-28 15:01:41 -0600

    create genre model and genre index view

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -2,4 +2,16 @@ class FilmFile < Sinatra::Base
   get '/films' do
     erb :films_index
   end
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :
+end
+
+class FilmFile < Sinatra::Base
+  get '/genres' do
+    @genres = Genre.all
+    erb :genres_index
+  end
+
 end
